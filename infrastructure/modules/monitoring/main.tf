9961a1f8962f05b5ca4032ef93516059e3c517dd
x091204 <akifmuhammed009@gmail.com>
2026-05-11 19:03:53 +0530

    updated helm chart with monitoring

--- infrastructure/modules/monitoring/main.tf
+++ infrastructure/modules/monitoring/main.tf
@@ -4,6 +4,12 @@ resource "kubernetes_namespace_v1" "monitoring" {
   }
 }
 
+resource "kubernetes_namespace_v1" "three-tier-dev" {
+  metadata {
+    name = "three-tier-dev"
+  }
+}
+
 terraform {
   required_providers {
     kubernetes = {
@@ -29,6 +35,13 @@ resource "helm_release" "monitoring" {
   values = [
     yamlencode({
         grafana = {
+            image = {
+                tag = "13.0.1"
+            }
+            
+            adminUser     = "admin"
+            adminPassword = "admin"
+
             service = {
                 type = "ClusterIP"
             }
@@ -49,5 +62,35 @@ resource "helm_release" "monitoring" {
   ]
   depends_on = [
     kubernetes_namespace_v1.monitoring
+  ]
+}
+
+resource "helm_release" "mongodb_exporter" {
+  name       = "mongodb-exporter"
+  namespace  = "three-tier-dev"
+
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "prometheus-mongodb-exporter"
+  version    = "4.22.0"
+
+  values = [
+    yamlencode({
+      mongodb = {
+        uri = "mongodb://username01:password01@mongodb-svc.three-tier-dev.svc.cluster.local:27017"
+      }
+
+      serviceMonitor = {
+        enabled = true
+        interval = "30s"
+        scrapeTimeout = "10s"
+
+        additionalLabels = {
+          release = "monitoring"
+        }
+      }
+    })
+  ]
+  depends_on = [
+    kubernetes_namespace_v1.three-tier-dev
   ]
 }
