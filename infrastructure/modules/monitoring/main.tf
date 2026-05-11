b9b7445d3b58cf60da2b94a678b756cc99a2588f
x091204 <akifmuhammed009@gmail.com>
2026-05-11 19:06:04 +0530

    updating monitoring stack

--- infrastructure/modules/monitoring/main.tf
+++ infrastructure/modules/monitoring/main.tf
@@ -4,11 +4,6 @@ resource "kubernetes_namespace_v1" "monitoring" {
   }
 }
 
-resource "kubernetes_namespace_v1" "three-tier-dev" {
-  metadata {
-    name = "three-tier-dev"
-  }
-}
 
 terraform {
   required_providers {
@@ -62,35 +57,5 @@ resource "helm_release" "monitoring" {
   ]
   depends_on = [
     kubernetes_namespace_v1.monitoring
-  ]
-}
-
-resource "helm_release" "mongodb_exporter" {
-  name       = "mongodb-exporter"
-  namespace  = "three-tier-dev"
-
-  repository = "https://prometheus-community.github.io/helm-charts"
-  chart      = "prometheus-mongodb-exporter"
-  version    = "4.22.0"
-
-  values = [
-    yamlencode({
-      mongodb = {
-        uri = "mongodb://username01:password01@mongodb-svc.three-tier-dev.svc.cluster.local:27017"
-      }
-
-      serviceMonitor = {
-        enabled = true
-        interval = "30s"
-        scrapeTimeout = "10s"
-
-        additionalLabels = {
-          release = "monitoring"
-        }
-      }
-    })
-  ]
-  depends_on = [
-    kubernetes_namespace_v1.three-tier-dev
   ]
 }
